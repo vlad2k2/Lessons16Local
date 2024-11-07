21302b3a9310dd88ffa1f7d7ff1a511a54b02a62
Vlad <vlad_rvz@ukr.net>
2024-11-07 14:33:56 +0200

    first locall commit

---
+++ src/main/java/Lessons16Local.java
@@ -0,0 +1,12 @@
+import app.models.Calculator;
+
+public class Lessons16Local {
+
+    public static void main(String[] args){
+
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.subtract(5,10));
+        System.out.println(calculator.add(5,10));
+    }
+
+}
